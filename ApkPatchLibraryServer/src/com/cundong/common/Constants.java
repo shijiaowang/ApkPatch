e9c6d9c8dd498d4b6b6172f0848d942690b8042f
杨国山 <973797893@qq.com>
2016-10-24 17:04:37 +0800

    第一次提交, Apk增量更新

---
+++ ApkPatchLibraryServer/src/com/cundong/common/Constants.java
@@ -0,0 +1,16 @@
+package com.cundong.common;
+
+public class Constants {
+
+	// 旧版本微博
+	public static final String OLD_APK = "/Users/cundong/Documents/SmartAppUpdates/weiboV4.5.0.apk";
+
+	// 新版本微博
+	public static final String NEW_APK = "/Users/cundong/Documents/SmartAppUpdates/weiboV4.5.5.apk";
+
+	// patch包存储路径
+	public static final String PATCH_FILE = "/Users/cundong/Documents/SmartAppUpdates/weibo.patch";
+
+	// 使用旧版本微博+patch包，合成的新包
+	public static final String OLD_2_NEW_APK = "/Users/cundong/Documents/SmartAppUpdates/weiboOld2New.apk";
+}
